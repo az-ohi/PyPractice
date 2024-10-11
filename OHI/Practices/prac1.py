7d6d0ee13d6d792979e35453957e2f2d67cbcfdc
az-ohi <ayubzawadohi@gmail.com>
2024-10-12 00:35:34 +0600

    new

---
+++ OHI/Practices/prac1.py
@@ -0,0 +1,9 @@
+print('''Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+''')
+import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)
