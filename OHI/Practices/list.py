a52027e50754ce10860dc10b6dd6f7b32a1c42c4
az-ohi <ayubzawadohi@gmail.com>
2024-10-12 14:27:07 +0600

    new

---
+++ OHI/Practices/list.py
@@ -0,0 +1,8 @@
+friend = ["Apple", "Grapes", 28, False, 63.025, "Ohi"]
+friend[2] = 27
+print(friend[2])
+print(friend[5])
+
+
+
+
---
+++ OHI/Practices/list_method.py
@@ -0,0 +1,28 @@
+friend = ["Apple", "Grapes", 28, False, 63.025, "Ohi"]
+print(friend)
+
+friend.append("Oli")
+print(friend)
+
+age = [1, 2,3,4,5,6,8,12,34,18,23,54,29]
+age.sort()
+print(age)
+age.reverse()
+print(age)
+age.insert(11,70)
+age.reverse()
+age.sort()
+age.reverse()
+print(age)
+
+
+
+
+
+
+
+
+
+
+
+
