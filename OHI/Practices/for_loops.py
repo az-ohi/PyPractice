1546de83f339cc9bbde48e91c3d5725ebf0e2e05
az-ohi <ayubzawadohi@gmail.com>
2024-10-15 01:01:16 +0600

    new

---
+++ OHI/Practices/for_loops.py
@@ -0,0 +1,39 @@
+li = ["Ohi", "Oli", "Abbu", "Ammu", 1, 2, 3, 4, 5, 6]
+
+for i in li:
+    print()
+
+st = "OHI is a good boy"
+for i in st:
+    print(i)
+
+for i in range(0, 51, 4):
+    print(i)
+
+
+# for loop with else
+
+i = [1,2,3,4,5,6,7,8,9,10]
+
+for x in i:
+    print(x)
+else:
+    print("Done")
+
+# break continue
+for i in range(1,10):
+    if(i == 5):
+        break
+    print(i)
+for i in range(1,10):
+    if(i == 5):
+        continue
+    print(i)
+
+# pass
+
+for i in range(4):
+    pass
+
+
+
---
+++ OHI/Practices/while_loops.py
@@ -0,0 +1,12 @@
+list = [12,18,47,27,"OHI",False, 4985, "OLI"]
+
+i = 0
+while (i< len(list)):
+    print(list[i])
+    i += 1
+
+
+x = 1
+while (x<10):
+    print("Ohi")
+    x += 1
