56e97a1c83cd5cbdf02c7982318463ac55430b2c
az-ohi <ayubzawadohi@gmail.com>
2024-10-14 21:02:19 +0600

    new

--- OHI/Practices/list_method.py
+++ OHI/Practices/list_method.py
@@ -4,16 +4,14 @@ print(friend)
 friend.append("Oli")
 print(friend)
 
-age = [1, 2,3,4,5,6,8,12,34,18,23,54,29]
+age = [1,2,3,4,5,6,8,12,34,18,23,54,29]
+age.insert(10,70)
 age.sort()
+age.remove(18)
 print(age)
-age.reverse()
-print(age)
-age.insert(11,70)
-age.reverse()
-age.sort()
-age.reverse()
-print(age)
+
+
+
 
 
 
---
+++ OHI/Practices/tuple.py
@@ -0,0 +1,18 @@
+a = (1, 2,3,34,3,45,4,5,345,"Ohi", False)
+print(a)
+print(type(a))
+
+no = a.count(5)
+print(no)
+
+b = (1,2,3)
+c = (3,4,5)
+con = b +c
+print(con)
+print(con.index(5))
+print(5 in con)
+print(len(con))
+print(min(con))
+print(max(con))
+
+
