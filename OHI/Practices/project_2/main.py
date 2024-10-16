150b4c68dbae1d232eb1349aa743ff80a62b4203
az-ohi <ayubzawadohi@gmail.com>
2024-10-16 23:32:47 +0600

    new

--- OHI/Practices/project_2/main.py
+++ OHI/Practices/project_2/main.py
@@ -6,11 +6,12 @@ n =random.randint(1,100)
 a = -1 #condition for entering into while loop
 guesses = 0
 while (a!=n):
+    guesses += 1
+
     a = int(input("Guess the number: "))
 
     if (a>n):
         print("\nLower Number please")
-        guesses += 1
 
     elif(a==n):
         print(f"\nYou have guessed the number {n} in {guesses} attempts")
